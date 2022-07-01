2bffb38cb4247ddedc7ec37c4bdf55c9bfb54a42
TaisyGomes <taisy.gomes001@gmail.com>
2022-06-30 22:46:41 -0300

    2 Day

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,27 @@
 fun main(){
+
+    /* FIRST IMPLEMENTACION
+    var idade = 20 * 365
+    var name = "Taisy"
+
     println("Happy Birthday!")
-    println("Taisy, ")
-    println("you are 20!")
+    println("${name},")
+    println("You are ${idade} days old, ${name}!")
+
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println("") 
+    */
+    
+    printBorder()
+    println("Happy Birthday, Taisy!")
+    printBorder()
+}
+
+fun printBorder(){
+    println("=====================")
 }
