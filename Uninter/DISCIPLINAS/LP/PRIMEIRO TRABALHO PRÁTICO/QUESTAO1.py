a28954f53ed863a755b6d832c977f790a8889b32
TaisyGomes <taisy.gomes001@gmail.com>
2022-09-04 23:40:34 -0300

    Atualização

--- Uninter/DISCIPLINAS/LP/PRIMEIRO TRABALHO PRÁTICO/QUESTAO1.py
+++ Uninter/DISCIPLINAS/LP/PRIMEIRO TRABALHO PRÁTICO/QUESTAO1.py
@@ -6,31 +6,31 @@ if quantidade > 0:
     
     if 0 < quantidade <= 4:  # condição para execução de valores até 4 unidades
     
-        total_sem_desconto = valor * quantidade
+        total_sem_desconto = valor * quantidade #calculo sem desconto
         print('O valor sem desconto foi: R$', total_sem_desconto)
         print('O valor com desconto foi: R$', total_sem_desconto, '(desconto 0%)')
 
     elif 5 <= quantidade <= 19:  # condição para execução de valores entre 5 e 19 unidades
 
-        total_sem_desconto = valor * quantidade
-        desconto = 0.03 * total_sem_desconto
-        total_com_desconto = total_sem_desconto - desconto
+        total_sem_desconto = valor * quantidade #calculo sem desconto
+        desconto = 0.03 * total_sem_desconto #calculo do desconto
+        total_com_desconto = total_sem_desconto - desconto #aplicando desconto e calculando
         print('O valor sem desconto foi: R$', total_sem_desconto)
         print('O valor com desconto foi: R$', total_com_desconto, '(desconto 3%)')
 
     elif 20 <= quantidade <= 99: # condição para execução de valores entre 20 e 99 unidades
 
-        total_sem_desconto = valor * quantidade
-        desconto = 0.06 * total_sem_desconto
-        total_com_desconto = total_sem_desconto - desconto
+        total_sem_desconto = valor * quantidade #calculo sem desconto
+        desconto = 0.06 * total_sem_desconto  #calculo do desconto
+        total_com_desconto = total_sem_desconto - desconto #aplicando desconto e calculando
         print('O valor sem desconto foi: R$', total_sem_desconto)
         print('O valor com desconto foi: R$', total_com_desconto, '(desconto 6%)')
 
     else: # se a quantidade for igual ou maior que R$100, entra nesse else
 
-        total_sem_desconto = valor * quantidade
-        desconto = 0.1 * total_sem_desconto
-        total_com_desconto = total_sem_desconto - desconto
+        total_sem_desconto = valor * quantidade #calculo sem desconto
+        desconto = 0.1 * total_sem_desconto  #calculo do desconto
+        total_com_desconto = total_sem_desconto - desconto #aplicando desconto e calculando
         print('O valor sem desconto foi: R$', total_sem_desconto)
         print('O valor com desconto foi: R$', total_com_desconto, '(desconto 10%)')
 else: # caso o usuário digite um valor inválido para a quantidade, entra nesse else
